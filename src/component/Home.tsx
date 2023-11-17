3efda8b2e1978dfc1256f370e6f4d71b84b94f61
ashok_suvvada <suvvadashok@gmail.com>
2023-11-17 23:40:20 +0530

    setup portfolio project

---
+++ src/component/Home.tsx
@@ -0,0 +1,11 @@
+import { Main } from "./main";
+import { Navbar } from "./navbar";
+
+export function Home(){
+    return(
+        <div>
+        <Navbar/>
+        <Main/>
+        </div>
+    )
+}
---
+++ src/component/main.tsx
@@ -0,0 +1,3 @@
+export function Main(){
+    return <h1>hiii...</h1>
+}
