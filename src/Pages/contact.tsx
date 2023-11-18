c52bd0799be9f09fd1ab6320dab7000c66394108
ashok_suvvada <suvvadashok@gmail.com>
2023-11-19 02:06:43 +0530

    add contact page

---
+++ src/Pages/contact.tsx
@@ -0,0 +1,9 @@
+export function Contact(){
+    return(
+        <div>
+            <p>Phone Number: 6305749515</p>
+            <p>email: ashokkumar.suvvada14@gmail.com</p>
+            <p></p>
+        </div>
+    )
+}
