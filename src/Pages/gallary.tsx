ca64a197fbe6003f6b50b4acbdf468c9d22749fd
ashok_suvvada <suvvadashok@gmail.com>
2023-11-22 16:34:58 +0530

    add gallery page

---
+++ src/Pages/gallary.tsx
@@ -0,0 +1,32 @@
+import cheek_1 from "/cheek1.jpg"
+import cheek_2 from "/cheek_2.jpg"
+import cheek_3 from "/cheek_3.jpg"
+import cheek_4 from "/cheek_4.jpg"
+import cheek_5 from "/cheek_5.jpg"
+import cheek_6 from "/cheek_6.jpg"
+import butterfly from "/butterfly.jpg"
+import butterfly_1 from "/butterfly_1.jpg"
+import butterfly_2 from "/butterfly_2.jpg"
+import butterfly_3 from "/butterfly_3.jpg"  
+import butterfly_4 from "/butterfly_4.jpg"
+import flower from "/flower.jpg"
+import flower_1 from "/flower_1.jpg"
+import flower_2 from "/flower_2.jpg"
+import flower_3 from "/flower_3.jpg"
+import flower_4 from "/flower_4.jpg"
+import tree_1 from "/tree_1.jpg"
+import tree_2 from "/tree_2.jpg"
+import tree_3 from "/tree_3.jpg"
+
+
+const a:string[] = [cheek_1, cheek_2, cheek_3, cheek_4, cheek_5, 
+    cheek_6, butterfly, butterfly_1, butterfly_2, butterfly_3, 
+    butterfly_4, flower, flower_1, flower_2, flower_3, flower_4, tree_1, tree_2, tree_3];
+
+export function Gallery(){
+    return(
+        <div className="grid grid-cols-3">
+            { a.map(i=> <img src={i} key={i} alt="hii"  /> )}
+        </div>
+    )
+}
