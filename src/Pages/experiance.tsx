7000a65f00091829adfb0a806eb894dcb1d4d9ba
ashok_suvvada <suvvadashok@gmail.com>
2023-11-22 16:35:20 +0530

    add basic experiance page

---
+++ src/Pages/experiance.tsx
@@ -0,0 +1,9 @@
+import ti from "../../public/ti.png"
+
+export function Experiance(){
+    return(
+       <div className="h-full bg-no-repeat bg-center bg-[url('../../public/ti.png')]">
+         <h1>Technoidentiy</h1>
+       </div>
+    )
+}
