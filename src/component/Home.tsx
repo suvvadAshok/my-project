b6455f2eb5379c629b61b472f3d1f367129c88bf
ashok_suvvada <suvvadashok@gmail.com>
2023-11-22 16:39:11 +0530

    add routing to home

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
 import { Main } from "./main";
 import { Navbar } from "./navbar";
 
 export function Home(){
     return(
-        <div className="flex flex-col">
+    <div className="flex flex-col h-screen w-screen">
         <Navbar/>
-        <Main/>
+        <div className="grow overflow-auto">
+            <Outlet/>
         </div>
+    </div>
     )
 }
