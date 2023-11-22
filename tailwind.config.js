0e0a3d104438a9cd5214725dab173fbdb1973b13
ashok_suvvada <suvvadashok@gmail.com>
2023-11-22 16:43:35 +0530

    add font family IBM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // 'ti_img': "url('./public/ti.png')",
+      },
+      fontFamily:{
+        IBMPlex:['IBM_Plex_Mono', "monospace"],
+      }
+    },
   },
   plugins: [],
 }
