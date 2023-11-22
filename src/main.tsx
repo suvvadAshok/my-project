122160d1724b4ffc3b5be69a88af4f5fe227b308
ashok_suvvada <suvvadashok@gmail.com>
2023-11-22 16:39:28 +0530

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,47 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
 import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Education } from './Pages/education';
+import { ErrorPage } from './Pages/errorPage';
+import { Contact } from './Pages/contact';
+import { Gallery } from './Pages/gallary';
+import { Experiance } from './Pages/experiance';
+import { Home } from './component/Home';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/home/education",
+        element: <Education />,
+      },
+      {
+        path: "/home/Experiance/Technoidentity",
+        element: <Experiance />,
+      },
+      {
+        path: "/home/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/home/Hobbies/Photography",
+        element: <Gallery />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
